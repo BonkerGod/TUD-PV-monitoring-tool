f4f74c3e1b7571fdcf38bf37e9f4f8cba6b9ea7a
Wessel Oosterkamp <woosterkamp@tuddelft.nl>
2026-05-07 10:23:55 +0200

    List in db

--- pyt-to-SQL.py
+++ pyt-to-SQL.py
@@ -7,6 +7,8 @@ import numpy as np
 import pandas as pd
 import ast
 from dateutil import parser
+import pgvector.psycopg2
+from sqlalchemy import create_engine
 
 with open('opet-supervisor-config.json') as f:
     opet_supervisor_config = json.load(f)
@@ -22,6 +24,8 @@ DB_PASS = "1234"
 DB_HOST = "localhost"
 DB_PORT = "5432"
 
+
+
 try:
     conn = psycopg2.connect(database=DB_NAME,
                             user =DB_USER,
@@ -33,6 +37,15 @@ except:
     print("Database not connected succesfully")
     
 cur = conn.cursor()
+
+# Create pgvector extension if it doesn't exist
+try:
+    cur.execute("CREATE EXTENSION IF NOT EXISTS vector;")
+    conn.commit()
+    print("pgvector extension loaded")
+except Exception as e:
+    print(f"Could not create pgvector extension: {e}")
+    print("Make sure pgvector is installed in PostgreSQL")
 
 #print(datetime.datetime.now().hour)
 
@@ -77,16 +90,27 @@ def adddata():
     )
     
     # Could not get the string away, but this may be for the better, because postgres expects array in {} and not in [].
-    # df = pd.read_csv("example-data/2024-12-20/config_2024-12-20T15-16-00/opet_results_curve_2024-12-20.csv", delimiter=",")
-    # df["v"] = df["v"].apply(ast.literal_eval)
-    # print(df['v'])
-    # print(type(df["v"].iloc[0]))
-    # df.to_csv("example-data/2024-12-20/config_2024-12-20T15-16-00/opet_results_curve2_2024-12-20.csv", sep=',', index= False)
+    df = pd.read_csv("example-data/2024-12-20/config_2024-12-20T15-16-00/opet_results_curve_2024-12-20.csv", delimiter=",")
+    # print(df['v'].iloc[0])
+    # print(type(df['v'].iloc[0]))
+    df["v"] = df["v"].apply(ast.literal_eval)
+    df['i'] = df['i'].apply(ast.literal_eval)
+    print(df['v'].iloc[0])
+    print(np.shape(df['v'].iloc[0]))
+    print(type(df["v"].iloc[0]))
+    #df.to_sql("pv_curve_test", conn, if_exists='append', index=False)
+    #df.to_json("example-data/2024-12-20/config_2024-12-20T15-16-00/opet_results_curve2_2024-12-20.json", orient="records", lines=True)
     
-    with open(data_file_path) as f:
-    #with open("C:/Users/wesse/OneDrive/Documenten/Tu Delft/EE3P1/Database/SQL/pv.csv") as f: #test
-        #cur.copy_expert("COPY pv_curve(measurement_time, scheduled_time, measurement_duration, module_name, mounted_on, v,i, azimuth, inclination, t_air, humidity, dewpoint, relative_pressure, wind_speed, wind_speed_spread, wind_direction, wind_direction_spread, irradiance) FROM STDIN WITH DELIMITER',' HEADER CSV", f)
-        cur.copy_expert("COPY pv_curve_test(measurement_time, scheduled_time, measurement_duration, module_id, v, i, g) FROM STDIN WITH DELIMITER',' HEADER CSV", f)
+    # with open(data_file_path) as f:
+    # #with open("example-data/2024-12-20/config_2024-12-20T15-16-00/opet_results_curve2_2024-12-20.csv") as f: #test
+    #     #cur.copy_expert("COPY pv_curve(measurement_time, scheduled_time, measurement_duration, module_name, mounted_on, v,i, azimuth, inclination, t_air, humidity, dewpoint, relative_pressure, wind_speed, wind_speed_spread, wind_direction, wind_direction_spread, irradiance) FROM STDIN WITH DELIMITER',' HEADER CSV", f)
+    #     cur.copy_expert("COPY pv_curve_test(measurement_time, scheduled_time, measurement_duration, module_id, v, i, g) FROM STDIN WITH DELIMITER',' HEADER CSV", f)
+    
+    data = df.to_numpy()
+    print(data[0])
+    for d in data:
+
+        cur.execute("INSERT into pv_curve_test VALUES (%s, %s, %s, %s, %s, %s, %s)", d)
     conn.commit()
 
 def count_entries(type):
@@ -151,8 +175,8 @@ def createtable(type):
             scheduled_time VARCHAR(255),
             measurement_duration VARCHAR(255),
             module_id VARCHAR(255),
-            v VARCHAR(2000),
-            i VARCHAR(2000),
+            v VECTOR(100),
+            i VECTOR(100),
             g float)"""
     try:
         cur.execute(command)
@@ -177,13 +201,19 @@ def downloadtable(file, type, datetime1, datetime2, module_name):
             cur.copy_expert(query, f)
         df = pd.read_csv(file)
         df['scheduled_time'] = pd.to_datetime(df['scheduled_time'])
-        print(df['scheduled_time'])
-        print(df.dtypes)
+        # print(df['scheduled_time'])
+        # print(df.dtypes)
         result = df.loc[(df['scheduled_time'] >= datetime.datetime.fromisoformat(datetime1)) & (df["scheduled_time"]<= datetime.datetime.fromisoformat(datetime2))]
         result = df.loc[(df['module_id'].isin(module_name))]
-        print(result)
+        #print(result)
   
   
-        
-downloadtable("test.csv", "curve_test", "2024-12-20 16:00:50-07:00", "2024-12-20 16:07:20-07:00", ["P-0000-01", "p-0234"])
+
+
+createtable("curve_test")
+adddata()
+#printtable("curve_test")
+downloadtable("test.json", "curve_test", "2024-12-20 00:00:00-07:00", "2024-12-21 00:00:00-07:00", ["P-0000-01", "module_2"])
+
+
 conn.close()
