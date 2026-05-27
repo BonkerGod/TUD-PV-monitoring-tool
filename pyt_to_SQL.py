4b33812432827785160cee10a707e7f6f73ae53d
Wessel Oosterkamp <woosterkamp@tuddelft.nl>
2026-05-27 10:56:35 +0200

    Added logging

--- pyt_to_SQL.py
+++ pyt_to_SQL.py
@@ -473,14 +473,14 @@ def download_table(file, type, datetime1, datetime2, module_name, conn, cur):
         type (string): Measurement type: 'pv_point' or 'pv_curve'.
         datetime1 (string): start datetime in string, example: "2024-12-20 16:00:50-07:00".
         datetime2 (string): end datetime in string, example: "2026-12-20 16:00:50-07:00".
-        module_name (list or string): List of the modules selected.
+        module_name (list): List of the modules selected.
         conn (_type_): Connection to the PostgreSQL database.
         cur (_type_): cursor for the PostgreSQL database.
     """
 
     dt1 = datetime.datetime.fromisoformat(datetime1)
     dt2 = datetime.datetime.fromisoformat(datetime2)
-    query = sql.SQL("COPY (SELECT * FROM " +type+ " LEFT JOIN weather ON "+type+".weather_id = weather.weather_id WHERE scheduled_time > %s AND scheduled_time < %s) TO STDOUT WITH DELIMITER ',' CSV HEADER ")
+    query = sql.SQL("COPY (SELECT * FROM " +type+ " LEFT JOIN weather ON "+type+".weather_id = weather.weather_id WHERE scheduled_time > %s AND scheduled_time < %s ORDER BY date_time DESC) TO STDOUT WITH DELIMITER ',' CSV HEADER ")
     with open(file, 'w') as f:
         formatted_query = cur.mogrify(query, [dt1,dt2]).decode('utf-8')
         cur.copy_expert(formatted_query, f)
@@ -555,8 +555,11 @@ def send_mail(error, config, receiver_email = ''):
         server.send_message(message)
         print("Email sent successfully")
         server.quit()
+        logging.debug(f"Email has been send")
+        
     except Exception as e:
         print(f"Error sending email: {e}")
+        logging.error(f"Error sending email : {e}")
      
         
 def error_detect(conn, cur, config):
@@ -687,12 +690,13 @@ def db_close(conn):
     conn.close()
 
 
-# conn, cur, mysql_conn, mysql_cur, config, data_path_base = init()
+conn, cur, mysql_conn, mysql_cur, config, data_path_base = init()
 # delete_table('weather', conn, cur)
 # create_table('weather', conn, cur)
 # # add_data('2026-05-26', conn, cur , mysql_conn, mysql_cur, config, data_path_base)
 # add_weather_data(download_weather_last24hours(1, mysql_conn, mysql_cur), conn, cur)
 # print_table('weather', conn, cur)
-# db_close(conn)
-update_loop()
+download_table('test1.csv', 'pv_point', "2026-05-25 16:00:50-07:00", "2026-12-20 16:00:50-07:00", ["My_solar_panel_1"], conn, cur)
+db_close(conn)
+#daily_loop()
 
