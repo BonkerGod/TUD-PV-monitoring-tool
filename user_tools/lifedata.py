ed152f56ed7f1849cb4ff839f9abbc62146ccaeb
Justen van Koppen <j.vankoppen-2@student.tudelft.nl>
2026-06-01 12:19:04 +0200

    logger

--- user_tools/lifedata.py
+++ user_tools/lifedata.py
@@ -4,6 +4,7 @@ from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import zoneinfo
+import datetime
 
 sys.path.insert(0, str(Path(__file__).resolve().parents[1] / "supervisor_tools"))
 
@@ -23,8 +24,9 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from matplotlib.animation import FuncAnimation
 import zoneinfo
-import datetime
+import matplotlib
 
+matplotlib.set_loglevel("warning")
 fig, ax1 = plt.subplots()
 ax2 = ax1.twinx()
 
@@ -51,7 +53,7 @@ labels = [l.get_label() for l in lines]
 ax1.legend(lines, labels, loc='upper left')
 
 def update(frame):
-    measurement = last_measurement(conn, cur, 'My_solar_panel_2')
+    measurement = last_measurement(conn, cur, 'My_solar_panel_1')
 
     times.append(measurement[0])      # datetime object
     voltages.append(measurement[4])   # voltage
